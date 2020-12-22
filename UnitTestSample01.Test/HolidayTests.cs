dc3f0a0de963aa87e95cdff2d0db5dfc69ce70a8
Wuu <coolwuu0207@gmail.com>
2020-12-22 14:22:20 +0800

    Add test case

--- UnitTestSample01.Test/HolidayTests.cs
+++ UnitTestSample01.Test/HolidayTests.cs
@@ -12,5 +12,13 @@ namespace UnitTestSample01.Test
             holiday.SetToday(new DateTime(2020, 12, 25));
             Assert.AreEqual("Merry X'Mas", holiday.SayXMas());
         }
+
+        [Test]
+        public void When_12_01_Should_Return_Correctly()
+        {
+            var holiday = new FakeHoliday();
+            holiday.SetToday(new DateTime(2020, 12, 01));
+            Assert.AreEqual("Very sad, Today is not X'Mas", holiday.SayXMas());
+        }
     }
 }
