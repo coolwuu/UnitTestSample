c8d58697638661ca0473be102da0394d9d525935
Wuu <coolwuu0207@gmail.com>
2020-12-22 14:24:17 +0800

    Refactor test cases

--- UnitTestSample01.Test/HolidayTests.cs
+++ UnitTestSample01.Test/HolidayTests.cs
@@ -5,20 +5,26 @@ namespace UnitTestSample01.Test
 {
     public class HolidayTests
     {
+        private FakeHoliday _holiday;
+
+        [SetUp]
+        public void Init()
+        {
+            _holiday = new FakeHoliday();
+        }
+
         [Test]
         public void When_12_25_Should_Return_Merry_XMas()
         {
-            var holiday = new FakeHoliday();
-            holiday.SetToday(new DateTime(2020, 12, 25));
-            Assert.AreEqual("Merry X'Mas", holiday.SayXMas());
+            _holiday.SetToday(new DateTime(2020, 12, 25));
+            Assert.AreEqual("Merry X'Mas", _holiday.SayXMas());
         }
 
         [Test]
         public void When_12_01_Should_Return_Correctly()
         {
-            var holiday = new FakeHoliday();
-            holiday.SetToday(new DateTime(2020, 12, 01));
-            Assert.AreEqual("Very sad, Today is not X'Mas", holiday.SayXMas());
+            _holiday.SetToday(new DateTime(2020, 12, 01));
+            Assert.AreEqual("Very sad, Today is not X'Mas", _holiday.SayXMas());
         }
     }
 }
