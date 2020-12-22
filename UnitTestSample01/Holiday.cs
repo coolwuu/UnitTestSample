64a7998bedc2942cc40f708382df42087b6be936
Wuu <coolwuu0207@gmail.com>
2020-12-22 14:14:32 +0800

    Extract Static object reference to a method and make it protected override.

--- UnitTestSample01/Holiday.cs
+++ UnitTestSample01/Holiday.cs
@@ -6,12 +6,17 @@ namespace UnitTestSample01
     {
         public string SayXMas()
         {
-            var today = DateTime.Today;
+            var today = GetToday();
             if (today.Month == 12 && today.Day == 25)
             {
                 return "Merry X'Mas";
             }
             return "Very sad, Today is not X'Mas";
+        }
+
+        protected virtual DateTime GetToday()
+        {
+            return DateTime.Today;
         }
     }
 }
