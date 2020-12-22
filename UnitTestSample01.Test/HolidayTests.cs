008714e09c7d7a14b488261f6a7851bec008c10a
Wuu <coolwuu0207@gmail.com>
2020-12-22 14:18:31 +0800

    Rewrite test and pass test

--- UnitTestSample01.Test/HolidayTests.cs
+++ UnitTestSample01.Test/HolidayTests.cs
@@ -6,9 +6,10 @@ namespace UnitTestSample01.Test
     public class HolidayTests
     {
         [Test]
-        public void IsTodayXMas()
+        public void When_12_25_Should_Return_Merry_XMas()
         {
-            var holiday = new Holiday();
+            var holiday = new FakeHoliday();
+            holiday.SetToday(new DateTime(2020, 12, 25));
             Assert.AreEqual("Merry X'Mas", holiday.SayXMas());
         }
 
