93b2a1470d44b2ee4fc80505a1358e316404f2dc
Wuu <coolwuu0207@gmail.com>
2020-12-22 14:13:18 +0800

    Add first failed test.

--- UnitTestSample01.Test/HolidayTests.cs
+++ UnitTestSample01.Test/HolidayTests.cs
@@ -1,7 +1,14 @@
-﻿namespace UnitTestSample01.Test
+﻿using NUnit.Framework;
+
+namespace UnitTestSample01.Test
 {
     public class HolidayTests
     {
-        
+        [Test]
+        public void IsTodayXMas()
+        {
+            var holiday = new Holiday();
+            Assert.AreEqual("Merry X'Mas", holiday.SayXMas());
+        }
     }
 }
