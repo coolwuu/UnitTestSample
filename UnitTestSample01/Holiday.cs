e474c84c6305ce0a5310da6afd72e67323c97d58
Wuu <coolwuu0207@gmail.com>
2020-12-22 14:09:03 +0800

    Add Holiday class

---
+++ UnitTestSample01/Holiday.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UnitTestSample01
+{
+    public class Holiday
+    {
+        public string SayXMas()
+        {
+            var today = DateTime.Today;
+            if (today.Month == 12 && today.Day == 25)
+            {
+                return "Merry X'Mas";
+            }
+            return "Very sad, Today is not X'Mas";
+        }
+    }
+}
