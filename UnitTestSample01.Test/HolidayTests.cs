9dadf97f3f27ae60b4331413ece385f63e4b5344
Wuu <coolwuu0207@gmail.com>
2020-12-22 14:26:41 +0800

    Refactor test cases

--- UnitTestSample01.Test/HolidayTests.cs
+++ UnitTestSample01.Test/HolidayTests.cs
@@ -17,19 +17,24 @@ namespace UnitTestSample01.Test
         public void When_12_25_Should_Return_Merry_XMas()
         {
             GivenToday(12, 25);
-            Assert.AreEqual("Merry X'Mas", _holiday.SayXMas());
+            ShouldSay("Merry X'Mas");
         }
 
         [Test]
         public void When_12_01_Should_Return_Correctly()
         {
             GivenToday(12, 01);
-            Assert.AreEqual("Very sad, Today is not X'Mas", _holiday.SayXMas());
+            ShouldSay("Very sad, Today is not X'Mas");
         }
         
         private void GivenToday(int month, int day)
         {
             _holiday.SetToday(new DateTime(2020, month, day));
+        }
+
+        private void ShouldSay(string expected)
+        {
+            Assert.AreEqual(expected, _holiday.SayXMas());
         }
     }
 }
